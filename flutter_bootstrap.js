a58624bbb86657c4791502d8c739f09e9599707e
Arina <arinakozonogova@gmail.com>
2025-06-20 18:46:51 +0500

    Deploy to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"148db22e342cdb37043aa26903375dd24bd207
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1498476619"
+    serviceWorkerVersion: "4112208643"
   }
 });
